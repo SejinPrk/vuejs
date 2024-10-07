aaa38bf643b786917cf0e74cbff75b14d3f9f7af
unknown <qkrwns9808@naver.com>
2024-10-07 10:05:10 +0900

    event handler: report button

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -4,23 +4,48 @@
     <div class="menu">
       <a v-for="a in menus" :key="a">{{ a }}</a>
     </div>
-    <div v-for="(products, i) in products" :key="i">
-      <h4>{{ products }}</h4>
+    <!-- <div v-for="(products, i) in products" :key="i">
+      <h4>{{ products[i] }}</h4>
       <p>XX 만원</p>
+    </div> -->
+    <div>
+      <h4>{{ products[0]}}</h4>
+      <p>50만원</p>
+      <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{reportNo[0]}} </span>
+    </div>
+    <div>
+      <h4>{{ products[1]}}</h4>
+      <p>60만원</p>
+      <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{reportNo[1]}}</span>
+    </div>
+    <div>
+      <h4>{{ products[2]}}</h4>
+      <p>70만원</p>
+      <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{reportNo[2]}} </span>
     </div>
   </div>
 </template>
 
 <script>
 
+
 export default {
   name: 'App',
   data() {
     return {
+      reportNo : [0, 0, 0],
       menus : ['Home', 'Shop', 'About'],
       products: ['역삼동 원룸', '천호동원룸', '마포구원룸'],
     }
   },
+  methods : {
+    // 함수 만드는 공간 
+    increase(index) {
+      this.reportNo[index] += 1; 
+    },
+    
+  },
+
   components: {
   }
 }
