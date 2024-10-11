ae2366f7cddd08669b59553409d2ef7d376a4d11
unknown <qkrwns9808@naver.com>
2024-10-11 15:15:50 +0900

    vuejs : instagram

---
+++ vuestagram/src/components/ContainerBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <PostIG :postings="postings[i]" v-for="(a, i) in postings" :key="i" />
+  </div>
+</template>
+
+<script>
+import PostIG from './PostIG.vue';
+
+export default {
+  components: {
+      PostIG,
+  },
+  props : {
+    postings: Array
+  }
+}
+</script>
+
+<style>
+
+</style>
