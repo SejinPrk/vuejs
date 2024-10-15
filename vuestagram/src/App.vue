1babfd406ddf06eae04030544e87833b71d43da4
unknown <qkrwns9808@naver.com>
2024-10-15 14:29:03 +0900

    tab

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -9,7 +9,7 @@
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <ContainerBox  :postings="postings"/>
+  <ContainerBox :postings="postings" :step="step"/>
   <button @click="showMore">더보기</button>
   
   <div class="footer">
@@ -18,6 +18,15 @@
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
+
+  
+  <!-- <div v-if="step == 0">내용0</div>
+  <div v-if="step == 1">내용1</div>
+  <div v-if="step == 2">내용2</div>
+  <button @click="step = 0">버튼0</button>
+  <button @click="step = 1">버튼1</button>
+  <button @click="step = 2">버튼2</button> -->
+
 </template>
 
 <script>
@@ -31,6 +40,7 @@ export default {
   name: 'App',
   data() {
     return {
+      step: 2,
       postings : postdata,
       moreCount : 0,
     }
--- vuestagram/src/components/ContainerBox.vue
+++ vuestagram/src/components/ContainerBox.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
-    <PostIG :postings="postings[i]" v-for="(a, i) in postings" :key="i" />
+
+    <div v-if="step == 0">
+      <PostIG :postings="postings[i]" v-for="(a, i) in postings" :key="i" />
+    </div>
   </div>
+
+  <!-- 필터선택페이지 -->
+  <div  v-if="step == 1">
+    <div class="upload-image"></div>
+    <div class="filters">
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+    </div>
+  </div>
+
+  <!-- 글작성페이지 -->
+  <div v-if="step == 2">
+    <div class="upload-image"></div>
+    <div class="write">
+      <textarea class="write-box">write!</textarea>
+    </div>
+  </div>
+  
 </template>
 
 <script>
@@ -12,7 +36,8 @@ export default {
       PostIG,
   },
   props : {
-    postings: Array
+    postings: Array,
+    step: Number
   }
 }
 </script>
